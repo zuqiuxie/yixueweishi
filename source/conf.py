4b1109dd56c8b0617c1335c05724ec97fc127222
binshao <50693793+zuqiuxie@users.noreply.github.com>
2023-03-11 14:12:38 +0800

    更新

--- source/conf.py
+++ source/conf.py
@@ -24,5 +24,5 @@ language = 'ZH_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sizzle'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']

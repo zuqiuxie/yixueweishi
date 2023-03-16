01e20244fe9efd7e845faae0c8f7f6c0098fbd81
binshao <50693793+zuqiuxie@users.noreply.github.com>
2023-03-16 13:34:17 +0800

    更新

--- build/html/app-data.js
+++ build/html/app-data.js
@@ -4,6 +4,3 @@
 
   sizzle.app_data = JSON.parse('{"glossary": {"document": null, "terms": {}}, "custom_data": {}}');
 })();
-
-<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>
-<script>LA.init({id:"K0FBgOiDXAd0EKO5",ck:"K0FBgOiDXAd0EKO5"})</script>

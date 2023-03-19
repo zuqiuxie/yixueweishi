9c1183369011d0a72788dce47797a1d339ee7658
binshao <50693793+zuqiuxie@users.noreply.github.com>
2023-03-19 11:17:20 +0800

    更新

--- source/conf.py
+++ source/conf.py
@@ -72,5 +72,6 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 html_js_files = [
-    'baidutongji.js'
+    'js/readmore.js',
+    'js/baidutongji.js'
 ]

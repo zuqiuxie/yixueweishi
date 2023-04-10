d8f111602be79f9f47d36f498f0ee6af4ae9854a
binshao <50693793+zuqiuxie@users.noreply.github.com>
2023-04-10 22:46:06 +0800

    更新

--- build/html/_static/js/baidutongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?aa95342ed41eba380e4ebb997621a136";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();

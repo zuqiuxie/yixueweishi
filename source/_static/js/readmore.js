95573d2d7a56a8960be9b429a1745079508a9059
binshao <50693793+zuqiuxie@users.noreply.github.com>
2023-05-29 20:33:14 +0800

    更新

---
+++ source/_static/js/readmore.js
@@ -0,0 +1,11 @@
+<script src="https://readmore.openwrite.cn/js/readmore.js" type="text/javascript"></script>
+<script>
+    const btw = new BTWPlugin();
+    btw.init({
+        id: 'container',
+        blogId: '31049-1685363084229-275',
+        name: '彬彬有礼的彬彬',
+        qrcode: 'https://raw.githubusercontent.com/zuqiuxie/lzk365/main/source/image/logo-binbinyouli.png',
+        keyword: 'yxws',
+    });
+</script>

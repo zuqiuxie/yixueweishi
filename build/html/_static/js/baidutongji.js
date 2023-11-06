453eee13afea5aa507449a1676de7c55228e133b
彬少 <dz303241@163.com>
2023-11-06 23:43:45 +0800

    更新

---
+++ build/html/_static/js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?aa95342ed41eba380e4ebb997621a136";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
